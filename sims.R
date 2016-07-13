906cb7de23db11bb050198926d18dc77a172a971
Gustavo Burin @ c3po <arietebio@gmail.com>
2016-07-13 14:37:55 -0300

    first analysis done, a few modifications in the original script

--- sims.R
+++ sims.R
@@ -1,4 +1,6 @@
 library(geiger)
+library(plyr)
+library(doMC)
 lapply(paste0("./R/",as.list(list.files("./R/"))), source)
 
 sProb <- c(0,2)  # pSpec = probability of speciation within the network
@@ -8,16 +10,18 @@ spProb <- c(0,2)  # lambda for tree simulation
 
 ##### Symmetrical transitions
 
-tree.num <- 1:1000
-pSpec[] <- runif(1000, sProb[1], sProb[2])
-q01[] <- runif(1000, qProb[1], qProb[2])
-lambda[] <- runif(1000, spProb[1], spProb[2])
-ntaxa[] <- round(runif(1000, tProb[1], tProb[2]))
-trees <- as.list(rep(NA, 1000))
-nets <- as.list(rep(NA, 1000))
+## tree.num <- 1:1000
+## pSpec <- runif(1000, sProb[1], sProb[2])
+## q01 <- runif(1000, qProb[1], qProb[2])
+## lambda <- runif(1000, spProb[1], spProb[2])
+## ntaxa <- round(runif(1000, tProb[1], tProb[2]))
+## trees <- as.list(rep(NA, 1000))
+## nets <- as.list(rep(NA, 1000))
 
-inputData <- data.frame(tree.num = tree.num, q01.sim = q01, q10.sim = q01, pSpec.sim = pSpec, lambda.sim = lambda, ntaxa = ntaxa)
+## inputData <- data.frame(tree.num = tree.num, q01.sim = q01, q10.sim = q01, pSpec.sim = pSpec, lambda.sim = lambda, ntaxa = ntaxa)
 
+## write.table(inputData, "./inputData.csv", quote = FALSE, row.names = FALSE, col.names = TRUE, sep = ",")
+inputData <- read.csv("./inputData.csv")
 
 sym.trans <- function(pars) {
     n <- pars[, 1]
@@ -30,7 +34,7 @@ sym.trans <- function(pars) {
     tt <- sim.bdtree(b = lambda, d = 0, stop = "taxa", n = ntaxa)
     tt <- drop.tip(tt, paste0("s", ntaxa))
     write.tree(tt, file = paste0("./trees/symtrans/tree_", n, "_symtrans.txt"))
-    nets <- simPhyloNetwork(tt, qRate = q01[x], sProb = pSpec)
+    nets <- simPhyloNetwork(tt, qRate = q01, sProb = pSpec)
     write.table(nets, file = paste0("./nets/symtrans/net_", n, "_symtrans.csv"), sep = ",", row.names = TRUE, col.names = TRUE, quote = FALSE)
 
     foo <- function(par) {
